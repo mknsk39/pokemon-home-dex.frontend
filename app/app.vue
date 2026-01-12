09785bccc015cb65f29e9abb4407de27111c4bc2
mknsk39 <252402208+mknsk39@users.noreply.github.com>
2026-01-12 10:42:19 +0000

    feat: Fixes #2 Vuetify のセットアップ

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container class="text-center mt-10">
+        <h1 class="text-h4 font-weight-bold mb-4">Nuxt + Vuetify 動作確認ページ</h1>
+        <v-btn color="primary" @click="toggleDark">
+          <v-icon start>mdi-theme-light-dark</v-icon>
+          Toggle Dark Mode
+        </v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+const toggleDark = () => {
+  const current = theme.global.name.value
+  const next = current === 'light' ? 'dark' : 'light'
+  theme.change(next)
+}
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,28 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  vite: {
+    plugins: [
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 1000,
+      },
+    },
+  },
 })
